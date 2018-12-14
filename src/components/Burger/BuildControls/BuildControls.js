68d58f7abb4c79b1605c144df9873af127ecc3b8
Hans-Christian Psaar <shopfreelancer@gmail.com>
2018-12-14 19:48:04 +0100

    added BuildControls for Burger. no functionality yet implemented.

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import classes from './BuildControls.css';
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+    {label: 'Salad', type: 'salad'},
+    {label: 'Bacon', type: 'bacon'},
+    {label: 'Cheese', type: 'cheese'},
+    {label: 'Meat', type: 'meat'},
+];
+
+const buildControls = (props) => (
+    <div className={classes.BuildControls}>
+        {controls.map((control,index) => (
+            <BuildControl label={control.label} type={control.type} key={index}/>
+        ))}
+    </div>
+);
+
+export default buildControls;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
-
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 class BurgerBuilder extends Component {
 
 
@@ -17,8 +17,8 @@ class BurgerBuilder extends Component {
     render() {
         return (
             <Aux>
-                <div>Controls</div>
                 <Burger />
+                <BuildControls/>´
             </Aux>
         )
     }
