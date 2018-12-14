df2886d3689007ecb78ba6cbc107614a9d13ff5a
Hans-Christian Psaar <shopfreelancer@gmail.com>
2018-12-14 15:26:21 +0100

    inital commit

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Aux from '../../hoc/Aux';
+//import classes from './Layout.css';
+
+const layout = (props) => (
+    <Aux>
+        <div>Toolbar, Sidedrawer, backdrop</div>
+        <main>
+            { props.children }
+        </main>
+    </Aux>
+);
+
+export default layout;
