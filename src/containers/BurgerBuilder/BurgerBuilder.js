7b9ab72f05a921c726246719c645f9fb3a7764a3
Hans-Christian Psaar <shopfreelancer@gmail.com>
2018-12-23 15:12:03 +0100

    added Orders Route and Container to display list of all orders added Order component to display single Order

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -47,32 +47,12 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        this.setState({ loading: true });
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Matt Customer',
-                address: {
-                    street: 'Test street 1',
-                    zipCode: '10179',
-                    town: 'Berlin',
-                    country: 'Germany'
-                }
-            }
-        };
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState({ loading: false, purchasing: false });
-            })
-            .catch(error => {
-                this.setState({ loading: false, purchasing: false });
-            });
 
         const queryParams = [];
         for (let i in this.state.ingredients) {
             queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
+        queryParams.push('price=' + this.state.totalPrice);
         const queryString = queryParams.join('&');
         this.props.history.push({ pathname: '/checkout', search: '?' + queryString });
     }
---
+++ src/containers/Orders/Orders.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Order from '../../components/Order/Order';
+
+class Orders extends Component {
+    render(){
+        return(<div><Order/></div>) ;
+    }
+}
+
+export default Orders;
