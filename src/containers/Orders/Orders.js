7fa74558eed4ffd04fea9d3184c172142734e20f
Hans-Christian Psaar <shopfreelancer@gmail.com>
2018-12-23 15:39:42 +0100

    adding list of orders in Orders.js

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
 import Order from '../../components/Order/Order';
+import axios from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 class Orders extends Component {
-    render(){
-        return(<div><Order/></div>) ;
+
+    state = {
+        orders: [],
+        loading: false
+    }
+
+    componentDidMount() {
+        this.setState({ loading: true });
+        axios.get('/orders.json')
+            .then(res => {
+                const fetchedOrders = [];
+                for (let key in res.data) {
+                    fetchedOrders.push({ ...res.data[key], id: key });
+                }
+                this.setState({ orders: fetchedOrders, loading: false });
+            })
+            .catch(err => {
+                this.setState({ loading: false });
+            })
+    }
+
+    render() {
+        return (
+            <div>
+                { this.state.orders.map(order => {
+                        return (<Order ingredients={ order.ingredients } price={ order.price } key={order.id}/>)
+                    }
+                ) }
+            </div>
+        );
     }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
