900ef1236b8e89871639511c7b2ad1d14ef67071
Hans-Christian Psaar <shopfreelancer@gmail.com>
2018-12-30 15:09:06 +0100

    connect redux to ContactData and Checkout

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -43,14 +43,7 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-
-        const queryParams = [];
-        for (let i in this.state.ingredients) {
-            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
-        }
-        queryParams.push('price=' + this.props.price);
-        const queryString = queryParams.join('&');
-        this.props.history.push({ pathname: '/checkout', search: '?' + queryString });
+        this.props.history.push('/checkout');
     }
 
     updatePurchaseState(ingredients) {
